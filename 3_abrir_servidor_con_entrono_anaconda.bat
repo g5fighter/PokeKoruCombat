597e5ee3e9feea07d9987e7312c92fcc86e2fd31
g5fighter <grmapal2@gmail.com>
2021-10-08 18:31:51 +0200

    fghfghfgh

---
+++ 3_abrir_servidor_con_entrono_anaconda.bat
@@ -0,0 +1,7 @@
+c:
+call C:\Users\grmap\anaconda3\Scripts\activate.bat C:\Users\grmap\anaconda3\envs\TwitchUtilities
+echo "Furula"
+d:
+cd D:\Proyectos\Programacion\HTML\PokeKoruCombat
+start "" http://localhost:5000
+npm start
